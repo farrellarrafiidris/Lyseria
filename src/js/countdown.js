15aeec8400d4e578cd9487cad7604b01de280724
farrellarrafidris <jshzhbsj@gmail.com>
2026-07-27 22:01:24 +0700

    feat: add grand opening countdown timer and closing statement section to homepage

---
+++ src/js/countdown.js
@@ -0,0 +1,39 @@
+// =============================
+// Countdown Setting
+// =============================
+
+// Tahun, Bulan, Tanggal, Jam, Menit
+const launchDate = new Date("2026-11-01T19:00:00");
+
+const countdown = document.getElementById("countdown");
+
+function updateCountdown() {
+
+    const now = new Date();
+
+    const distance = launchDate - now;
+
+    if (distance <= 0) {
+
+        countdown.innerHTML = "✨ Now Live";
+
+        return;
+
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    countdown.innerHTML =
+        `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+}
+
+updateCountdown();
+
+setInterval(updateCountdown, 1000);
