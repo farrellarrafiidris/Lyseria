28a1d8235d8900d1431ec64485db034352896fd0
farrellarrafidris <jshzhbsj@gmail.com>
2026-09-11 23:04:21 +0700

    style: remove background color from catalog product cards

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -23,7 +23,7 @@ async function loadCollections() {
 
                 cards += `
                     <div class="${cardClass} group">
-                        <div class="relative overflow-hidden rounded-3xl bg-pearl">
+                        <div class="relative overflow-hidden rounded-3xl">
                             <img
                                 src="${product.images[0]}"
                                 alt="${product.name}"
